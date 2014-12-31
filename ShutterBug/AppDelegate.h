84d5465416326700192329548b8bea9412535f75
Andrew Codispoti <andrewcodispoti@Andrews-Mac-mini.local>
2014-12-30 19:57:50 -0500

    Initial Commit

---
+++ ShutterBug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShutterBug
+//
+//  Created by Andrew Codispoti on 2014-12-30.
+//  Copyright (c) 2014 Andrew Codispoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShutterBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ShutterBug
+//
+//  Created by Andrew Codispoti on 2014-12-30.
+//  Copyright (c) 2014 Andrew Codispoti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
